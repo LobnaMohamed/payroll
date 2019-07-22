2b6e724aeb980707984005d60092bd64bdb64196
LobnaMohamed <lobna92_19@hotmail.com>
2019-07-22 12:49:30 +0200

    insert timesheet function in refined database

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 	//include 'connect.php';
 	// --------------connection to database function-------------
 	function connect(){
-        $servername = "LOBNA-PC";
+		$servername = "LOBNA";
         // $username = "username";
         // $password = "password";
         
         try {
 			//$con = new PDO("sqlsrvr:host=$servername;dbname=wages" );
-			$con = new PDO("sqlsrv:Server=$servername;Database=payroll");
+			$con = new PDO("sqlsrv:Server=$servername;Database=payroll_new");
             // set the PDO error mode to exception
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //echo "Connected successfully"; 
@@ -308,10 +308,16 @@
 		$con = connect();
 		$sql= '';		
 		if(!empty($_POST['timesheetDate'])){
-			$sql = "select t.*,e.currentCode,e.empName
-					from employee e,timesheet t
-					where t.emp_id = e.ID
-							and month(t.sheetDate)= month('".$_POST['timesheetDate']."')";	
+			// $sql = "select t.*,e.currentCode,e.empName
+			// 		from employee e,timesheet t
+			// 		where t.emp_id = e.ID
+			// 				and month(t.sheetDate)= month('".$_POST['timesheetDate']."')";	
+			//--------------------------------------------------------------
+			//payroll_new database
+			$sql= "select t.*,e.currentCode,e.empName,empt.*
+					from timesheets t inner join empTimesheet empt on t.ID = empt.TS_id 
+						inner join employee e on empt.emp_id = e.ID
+					and month(t.sheetDate)=  month('".$_POST['timesheetDate']."')";
 		}
 		if(!empty($_POST['search'])){
 			$sql .= " and (e.currentCode like '%". $_POST['search'] ."%' OR e.empName like '%". $_POST['search'] ."%')";	
@@ -321,58 +327,58 @@
 		$stmt->execute();
 		$result = $stmt->fetchAll();
 		if(! $result ){
-			// $output = "
-			// <tr>
-			// <td colspan='13' class='alert alert-warning'> 
-			// <strong>لا يوجد حصر أيام الحضور بهذا التاريخ.. لادخال الحصر اضغط هنا</strong><a href='#'>here</a>
-			// </td></tr>";
+			$output = "
+			<tr>
+			<td colspan='13' class='alert alert-warning'> 
+			<strong>لا يوجد حصر أيام الحضور بهذا التاريخ.. لادخال الحصر اضغط هنا</strong><a href='#'>here</a>
+			</td></tr>";
 			
-			$sql = "select  e.empName,e.currentCode,e.ID
-			from	employee e";
+		// 	$sql = "select  e.empName,e.currentCode,e.ID
+		// 	from	employee e";
 
-		$stmt = $con->prepare($sql);
-		$stmt->execute();
-		$result = $stmt->fetchAll();
-		foreach($result as $row){
-			$empindex = $row['ID'];
-			$output .= "<tr>
-				<td>".  $row['currentCode']. "</td>
-				<td>".  $row['empName']. "</td>
-				<td>". $_POST['timesheetDate']. "</td>
-				<input name='emp_id' type='hidden' value=".$row['ID'].">
-				<td>
-					<input  class='form-control' name='presence_days[".$row['ID']."]' value=30>
-				</td> 
-				<td>
-					<input  class='form-control' name='sickLeave_days[".$row['ID']."]' value=0>
-				</td>  
-				<td>
-					<input class='form-control' name='deduction_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='absence_days[".$row['ID']."]' value=0>
-				</td> 
-				<td>
-					<input class='form-control' name='annual_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='casual_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='manufacturing_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='shift_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='overnight_days[".$row['ID']."]' value=0>
-				</td>
-				<td>
-					<input class='form-control' name='notes[".$row['ID']."]'>
-				</td>
+		// $stmt = $con->prepare($sql);
+		// $stmt->execute();
+		// $result = $stmt->fetchAll();
+		// foreach($result as $row){
+		// 	$empindex = $row['ID'];
+		// 	$output .= "<tr>
+		// 		<td>".  $row['currentCode']. "</td>
+		// 		<td>".  $row['empName']. "</td>
+		// 		<td>". $_POST['timesheetDate']. "</td>
+		// 		<input name='emp_id' type='hidden' value=".$row['ID'].">
+		// 		<td>
+		// 			<input  class='form-control' name='presence_days[".$row['ID']."]' value=30>
+		// 		</td> 
+		// 		<td>
+		// 			<input  class='form-control' name='sickLeave_days[".$row['ID']."]' value=0>
+		// 		</td>  
+		// 		<td>
+		// 			<input class='form-control' name='deduction_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='absence_days[".$row['ID']."]' value=0>
+		// 		</td> 
+		// 		<td>
+		// 			<input class='form-control' name='annual_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='casual_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='manufacturing_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='shift_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='overnight_days[".$row['ID']."]' value=0>
+		// 		</td>
+		// 		<td>
+		// 			<input class='form-control' name='notes[".$row['ID']."]'>
+		// 		</td>
 					
-			</tr>";
-		}
+		// 	</tr>";
+		// }
 			
 		}else{
 			foreach($result as $row){
@@ -411,10 +417,14 @@
 		// 				and month(t.sheetDate) = month(getdate())";
 		
 		if(!empty($_POST['timesheetDate'])){
-			$sql = "select t.*,e.currentCode,e.empName
-					from employee e,timesheet t
-					where t.emp_id = e.ID
-							and month(t.sheetDate)= month('".$_POST['timesheetDate']."')";	
+			// $sql = "select t.*,e.currentCode,e.empName
+			// 		from employee e,timesheet t
+			// 		where t.emp_id = e.ID
+			// 				and month(t.sheetDate)= month('".$_POST['timesheetDate']."')";	
+			$sql= "select t.*,e.currentCode,e.empName,empt.*
+					from timesheets t inner join empTimesheet empt on t.ID = empt.TS_id 
+						inner join employee e on empt.emp_id = e.ID
+					and month(t.sheetDate)=  month('".$_POST['timesheetDate']."')";
 		}
 		if(!empty($_POST['search'])){
 			$sql .= " and (e.currentCode like '%". $_POST['search'] ."%' OR e.empName like '%". $_POST['search'] ."%')";	
@@ -1052,11 +1062,7 @@
 	function insertTimesheet(){
 		$con = connect();
 		$timesheetDate =$_POST['timesheetDate'];
-		$getlastTSID_sql = "select max(ID) from timesheet";
-		$stmt = $con->prepare($getlastTSID_sql);
-		$stmt->execute();
-		$lastID = $stmt->fetchColumn()+1;
-		echo $lastID;
+		
 
 		//---------------timesheet INSERTION---------------------
 		foreach ($_POST['presence_days'] as $empID => $value) {
@@ -1077,9 +1083,19 @@
 
 			//if($value !=30){
 				//echo $notes;
-				$sql = "insert into timesheet(ID,emp_id,sheetDate,presence_days,sickLeave_days,deduction_days,absence_days,annual_days,
+				$timesheetsql = "insert into timesheets(sheetDate) values('$timesheetDate')";
+				$stmt = $con->prepare($timesheetsql);
+				$stmt->execute();
+
+				$getlastTSID_sql = "select max(ID) from timesheets";
+				
+				$stmt = $con->prepare($getlastTSID_sql);
+				$stmt->execute();
+				$lastID = $stmt->fetchColumn();
+				echo $lastID;
+				$sql = "insert into empTimesheet(TS_id,emp_id,presence_days,sickLeave_days,deduction_days,absence_days,annual_days,
 								casual_days,manufacturing_days,overnight_days,shift_days,notes) 
-						values ('$lastID','$empID','$timesheetDate','$value','$sickLeave','$deduction','$absence','$annual',
+						values ('$lastID','$empID','$value','$sickLeave','$deduction','$absence','$annual',
 								'$casual','$manufacturing','$overnight','$shift','$notes')";
 				echo $sql;
 				$stmt = $con->prepare($sql);
