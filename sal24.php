ea21ca5edb43dc31df3b83f336d7fd8f1b056c52
Lobna mahmoud <lobna@AMOC.COM>
2022-08-08 14:37:30 +0200

    sal 24 file session

--- sal24.php
+++ sal24.php
@@ -1,15 +1,47 @@
 <?php
+    // timesheet file
+    // session_start();
+    // if(isset($_SESSION['navigationDate'])){
+	// 	echo "Welcome" . $_SESSION['navigationDate'];
+	// 	//exit();
+	// }else{
+    //     echo "no";
+    // }
+
+
+session_start();
+if(isset($_POST['navigationDate']) && isset($_POST['timesheetinsertBtn'])){
+    $_SESSION['navigationDate'] = $_POST['navigationDate'];
+    //echo "yes";
+    if(isset($_POST['timesheetinsertBtn'])){
+        header('Location: timesheetinsertion.php');//redirect
+    }
+    elseif(isset($_POST['shiftinsertBtn'])){
+        header('Location: shiftinsertion.php');//redirect
+    } 
+    elseif(isset($_POST['overnightinsertBtn'])){
+        header('Location: overnightinsertion.php');//redirect
+    }
+    elseif(isset($_POST['timesheetRevisionBtn'])){
+        header('Location: timesheet.php');//redirect
+    }
+    elseif(isset($_POST['sickleavesinsertBtn'])){
+        header('Location: sickleavesinsertion.php');//redirect
+    }
+}else{
+    echo "fail";
+}
+
 include 'header.php';
+     
+     
 ?>
 <div class="container">
-    <!-- <header class="row text-center">
-		<img class= "logo col-sm-1" src="images/amoc2.png">
-  	    <h1 class="col-sm-4 col-sm-offset-2 ">نموذج الاجـــــازة</h1>
-    </header> -->
+
 	<div class="page-header text-center">
-        <h1>salary 24th</h1>
+        <h1>deductions 24th</h1>
     </div>
-    <form  method="POST" action="add.php" id="navigation">
+    <form  method="POST" action ="<?php echo $_SERVER['PHP_SELF'] ?>" id="navigation">
         <div class="row">
             <div class="col-sm-4 col-sm-offset-4">
                 <div class="input-group">
@@ -18,30 +50,75 @@ include 'header.php';
                 </div><!-- /input-group -->
             </div><!-- /.col-lg-6 -->
         </div><!-- /.row -->
-
-        <div class="btn-group btn-group-justified" role="group" aria-label="...">
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Left</button>
+        <div class= "row">
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="submit" name ="timesheetinsertBtn" class="btn btn-default form-control">
+                <a href="timesheetinsertion.php">ادخال الحــصر </a>
+                    
+                </button>
             </div>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Middle</button>
+            <div class="col-sm-2">
+                <label ></label>
+                <button  type="submit" name ="shiftinsertBtn"  class="btn btn-default form-control">
+                    <a href="shiftinsertion.php">ادخال الوردية </a>
+                </button>
             </div>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Right</button>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="submit" name ="overnightinsertBtn" class="btn btn-default form-control">
+                    <a href="overnightinsertion.php">ادخال النوباتجية </a>
+                </button>
+            </div>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="submit" name ="sickleavesinsertBtn" class="btn btn-default form-control">
+                    <a href="sickleavesinsertion.php">ادخال المرضى </a>
+                </button>
+            </div>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="submit" name ="timesheetRevisionBtn"class="btn btn-default form-control">
+                    <a href="timesheet.php">مراجعة الحـــصر</a>
+                </button>
             </div>
         </div>
-
-        <div class="btn-group btn-group-justified" role="group" aria-label="...">
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Left</button>
+       
+        <div class= "row">
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="button" class="btn btn-default form-control">
+                <a href="timesheetinsertion.php">ادخال  جميع الاستقطاعات </a>
+                </button>
             </div>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Middle</button>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="button" class="btn btn-default form-control">
+                    <a href="shiftinsertion.php">ادخال استقطاع  </a>
+                </button>
             </div>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-default">Right</button>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="button" class="btn btn-default form-control">
+                    <a href="overnightinsertion.php">مراجعة الاستقطاعات</a>
+                </button>
             </div>
+        </div>
+        <div class= "row">
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="button" class="btn btn-default form-control">
+                <a href="timesheetinsertion.php">ادخال الاستحقاقات </a>
+                </button>
             </div>
+            <div class="col-sm-2">
+                <label ></label>
+                <button type="button" class="btn btn-default form-control">
+                    <a href="shiftinsertion.php"> مراجعةالاستحقاقات</a>
+                </button>
+            </div>
+        </div>
+       
 
     </form>
 </div>
